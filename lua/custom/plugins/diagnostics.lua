d0009af459749c1c62f0aa61453ccd53b6f77e43
pcristin <xxxokzxxx@protonmail.com>
2026-05-25 15:22:39 +0300

    Stabilize Trouble diagnostics view

--- lua/custom/plugins/diagnostics.lua
+++ lua/custom/plugins/diagnostics.lua
@@ -4,15 +4,18 @@ return {
     cmd = 'Trouble',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     keys = {
-      { '<leader>xx', '<cmd>Trouble diagnostics toggle<CR>', desc = 'Diagnostics' },
-      { '<leader>xX', '<cmd>Trouble diagnostics toggle filter.buf=0<CR>', desc = 'Buffer diagnostics' },
+      { '<leader>xx', '<cmd>Trouble diagnostics toggle focus=false<CR>', desc = 'Diagnostics' },
+      { '<leader>xX', '<cmd>Trouble diagnostics toggle focus=false filter.buf=0<CR>', desc = 'Buffer diagnostics' },
       { '<leader>xs', '<cmd>Trouble symbols toggle focus=false<CR>', desc = 'Document symbols' },
       { '<leader>xl', '<cmd>Trouble lsp toggle focus=false win.position=right<CR>', desc = 'LSP definitions/references' },
       { '<leader>xL', '<cmd>Trouble loclist toggle<CR>', desc = 'Location list' },
       { '<leader>xQ', '<cmd>Trouble qflist toggle<CR>', desc = 'Quickfix list' },
+      { '<leader>xq', '<cmd>Trouble close<CR>', desc = 'Close Trouble' },
     },
     opts = {
-      focus = true,
+      auto_preview = false,
+      focus = false,
+      restore = false,
     },
   },
   {
